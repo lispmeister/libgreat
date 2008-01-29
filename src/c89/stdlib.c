2c54ef3abb4aaa7e3f99f8fd7526d75d0f91fab6
kate <kate@8b4d562f-fecd-dc11-9224-0002b3ef260b>
2008-01-29 22:13:48 +0000

    More nasty behaviour, and annotations quoting chapter and verse.

--- src/c89/stdlib.c
+++ src/c89/stdlib.c
@@ -11,37 +11,61 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
-#include <errno.h>
 
 #include "wrap.h"
 #include "../shared/random.h"
 
+/* TODO paragraph numbers for P? */
+
+/* C89 4.10.3.3 The malloc function */
 void *
 malloc(size_t size)
 {
+	/* P? ...either a null pointer */
 	if(!great_random_success()) {
-		errno = ENOMEM;
 		return NULL;
 	}
 
+	/* P? ...or a pointer to the allocated space */
 	return great_c89.malloc(size);
 }
 
-/* XXX No mention of errno in the standard? */
+/* C89 4.10.3.4 The realloc function */
 void *
 realloc(void *ptr, size_t size)
 {
 	void *p;
 
+	/* P? If ptr is a null pointer, the realloc function
+	 *    behaves like the malloc function for the specified size. */
 	if(ptr == NULL) {
 		return malloc(size);
 	}
 
+	/* P? If size is zero and ptr is not a null pointer, the object
+	 *    it points to is freed. */
+	if(size == 0) {
+		free(ptr);
+
+		/* C89 does not enforce that NULL is returned (I think...) */
+		switch(great_random_choice(2)) {
+		case 0:
+			return NULL;
+
+		case 1:
+			return ptr;
+
+		default:
+			assert(!"unrecognised great random choice");
+		}
+	}
+
 	if(!great_random_success()) {
-		errno = ENOMEM;
 		return NULL;
 	}
 
+	/* P? The realloc function returns either a null pointer or a pointer to
+	 *    the possibly moved allocated space. */
 	switch(great_random_choice(2)) {
 	case 0:
 		/* possibly the same address */
@@ -54,10 +78,11 @@ realloc(void *ptr, size_t size)
 
 			p = malloc(size);
 			if(!p) {
-				errno = ENOMEM;
 				return NULL;
 			}
 
+			/* P? The contents of the object shall be unchanged up to the
+			 *    lesser of the new and old sizes. */
 			memcpy(p, ptr, size);
 			free(ptr);
 
